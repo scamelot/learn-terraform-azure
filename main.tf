08221d42e361874ec62e8ee3b920691b888902a7
Steve Scammon <stevescammon@me.com>
2022-11-18 19:38:21 -0500

    winlinux

---
+++ main.tf
@@ -0,0 +1,180 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+provider "azurerm" {
+  features {
+     resource_group {
+       prevent_deletion_if_contains_resources = false
+     }
+}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name = "myTFvnet"
+    address_space = ["10.0.0.0/16"]
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+    name = "internal"
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+    name = "vm_public_ip"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    allocation_method = "Dynamic"
+}
+
+resource "azurerm_public_ip" "win_public_ip" {
+    name = "vm_win_public_ip"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    allocation_method = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+    name = "myTFnic"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+      name = "internal"
+      subnet_id = azurerm_subnet.subnet.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id = azurerm_public_ip.public_ip.id
+    }
+}
+
+//windows nic
+resource "azurerm_network_interface" "winnic" {
+    name = "myTFWinnic"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+      name = "internal"
+      subnet_id = azurerm_subnet.subnet.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id = azurerm_public_ip.win_public_ip.id
+    }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+    name = "myTFnsg"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name = "allow_ssh_sg"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name = "allow_rdp_sg"
+        priority = 101
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "3389"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "association"{
+    network_interface_id = azurerm_network_interface.nic.id
+    network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface_security_group_association" "win_association"{
+    network_interface_id = azurerm_network_interface.winnic.id
+    network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "linuxVM1" {
+    name = "linux-vm-1"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    size = "Standard_B1s"
+    admin_username = "adminuser"
+
+    network_interface_ids = [
+        azurerm_network_interface.nic.id,
+    ]
+
+    admin_ssh_key {
+      username = "adminuser"
+      public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "windowsVM1" {
+    name = "windows-vm-1"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    size = "Standard_B1s"
+    admin_username = "adminuser"
+    admin_password = "hello!World"
+
+    network_interface_ids = [
+        azurerm_network_interface.winnic.id,
+    ]
+
+    os_disk {
+        name = "win-vm-os-disk"
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "windows-10"
+    sku       = "19h2-pro-g2"
+    version   = "latest"
+  }
+  enable_automatic_updates = true
+}
+
+output "public_ip" {
+    value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "win_public_ip" {
+    value = azurerm_public_ip.win_public_ip.ip_address
+}
