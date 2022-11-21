24cbca0cf879c4a05a6cd2dc82de5104cf34a963
Steve Scammon <stevescammon@me.com>
2022-11-20 19:14:57 -0500

    rdp

--- main.tf
+++ main.tf
@@ -103,6 +103,17 @@ resource "azurerm_network_security_group" "nsg" {
         source_address_prefix = "*"
         destination_address_prefix = "*"
     }
+    security_rule {
+        name = "allow_rdp_sg_out"
+        priority = 102
+        direction = "Outbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "3389"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 resource "azurerm_network_interface_security_group_association" "association"{
